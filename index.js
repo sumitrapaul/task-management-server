b9dea966bb450daf01291460644318f67f80a3c0
Sumitra Paul <sumitra.cse.just2020@gmail.com>
2023-12-22 19:54:11 +0600

    update data

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
  
-    await client.connect();
+    // await client.connect();
    
    //database collection
    const taskCollection = client.db("taskDB").collection("tasks");
